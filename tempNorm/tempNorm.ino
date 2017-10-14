e5473ed746ecf6e9d1e77114d39535f710c19006
milescobb1 <miles.cobb@gmail.com>
2017-10-14 11:03:11 -0700

    temperature normalizing

---
+++ tempNorm/tempNorm.ino
@@ -0,0 +1,24 @@
+int tempPin = 0;
+int tempMax = 84;
+int tempMin = 76;
+int relay = 1;
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  while(readTemperature() < tempMax) {
+    digitalWrite(relay, HIGH);
+    delay(1000);
+  }
+  while(readTemperature() > tempMin) {
+    digitalWrite(relay, LOW);
+    delay(1000);
+  }
+}
+
+float readTemperature() {
+  float temperature = analogRead(tempPin);
+  temperature = (temperature * tempScale - .5) * 100.0;
+  return (tempCalibR * tempCalibS * (temperature * (9.0 / 5.0) + 32.0) - tempOffset);
+}
