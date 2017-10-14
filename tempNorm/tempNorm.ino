557f9a772cd6e30cd28250cbdc9cb32412577935
milescobb1 <miles.cobb@gmail.com>
2017-10-14 11:06:46 -0700

    calibration changes

--- tempNorm/tempNorm.ino
+++ tempNorm/tempNorm.ino
@@ -2,6 +2,12 @@ int tempPin = 0;
 int tempMax = 84;
 int tempMin = 76;
 int relay = 1;
+
+int tempScale = 1;
+
+int tempCalibS = 1;
+
+int tempOffset = 1;
 void setup() {
   // put your setup code here, to run once:
 }
@@ -20,5 +26,5 @@ void loop() {
 float readTemperature() {
   float temperature = analogRead(tempPin);
   temperature = (temperature * tempScale - .5) * 100.0;
-  return (tempCalibR * tempCalibS * (temperature * (9.0 / 5.0) + 32.0) - tempOffset);
+  return (tempCalibS * (temperature * (9.0 / 5.0) + 32.0) - tempOffset);
 }
