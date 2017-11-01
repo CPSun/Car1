9626d55a4ff1f95133d2020fb824142065b1a789
milescobb1 <miles.cobb@gmail.com>
2017-11-01 16:42:21 -0700

    latest version

--- tempNorm/tempNorm.ino
+++ tempNorm/tempNorm.ino
@@ -1,30 +1,37 @@
 int tempPin = 0;
-int tempMax = 84;
-int tempMin = 76;
-int relay = 1;
+int tempMax = 83;
+int tempMin = 77;
+int relay = 7;
 
+float increment = .5;
 int tempScale = 1;
 
 int tempCalibS = 1;
 
-int tempOffset = 0;
+float tempOffset = 7;
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
 }
 
 void loop() {
   while(readTemperature() < tempMax) {
-    digitalWrite(relay, HIGH);
-    delay(1000);
+    digitalWrite(relay, LOW);
+    delay(increment*1000.0);
   }
   while(readTemperature() > tempMin) {
-    digitalWrite(relay, LOW);
-    delay(1000);
+    digitalWrite(relay, HIGH);
+    delay(increment*1000.0);
   }
+  delay(500);
 }
 
 float readTemperature() {
-  float temperature = analogRead(tempPin);
-  temperature = (temperature * tempScale - .5) * 100.0;
-  return (tempCalibS * (temperature * (9.0 / 5.0) + 32.0) - tempOffset);
+  int reading = analogRead(tempPin);  
+  float voltage = reading * 5.0;
+  voltage /= 1024.0; 
+  float temperatureC = (voltage - 0.5) * 100;
+  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0 + tempOffset;
+  Serial.println(temperatureF);
+  return temperatureF;
 }
